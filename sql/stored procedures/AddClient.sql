4603b192ebcc88206b7e9f3f227f0fa98a2df8dc
M B <mbagnsk@hotmail.com>
2022-12-03 23:42:47 +0100

    try catch commit rollback implemented

--- sql/stored procedures/AddClient.sql
+++ sql/stored procedures/AddClient.sql
@@ -1,17 +1,31 @@
-CREATE PROCEDURE AddClient
+USE [WarehouseManagerDB]
+GO
+/****** Object:  StoredProcedure [dbo].[AddClient]    Script Date: 03.12.2022 23:41:11 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[AddClient]
 	@Company nvarchar(100), 
 	@Nip numeric,
 	@Street nvarchar(50),
-	@BuildingNubmer numeric,
+	@BuildingNumber numeric,
 	@LocalNumber numeric,
 	@City nvarchar(50),
 	@ZIPCode nvarchar(6),
 	@Email nvarchar(50),
 	@PhoneNumber numeric
 AS
-INSERT INTO WarehouseManagerDB.dbo.Customers (Company, Nip, Street, BuildingNumber, LocalNumber, City, ZIPCode, Email, PhoneNumber)
-VALUES(@Company, @Nip, @Street, @BuildingNumber, @LocalNumber, @City, @ZIPCode, @Email, @PhoneNumber)
-GO
+BEGIN TRY
+	BEGIN TRAN
+	INSERT INTO WarehouseManagerDB.dbo.Customers (Company, Nip, Street, BuildingNumber, LocalNumber, City, ZIPCode, Email, PhoneNumber)
+	VALUES(@Company, @Nip, @Street, @BuildingNumber, @LocalNumber, @City, @ZIPCode, @Email, @PhoneNumber)
+	COMMIT TRAN
+END TRY
+BEGIN CATCH
+	ROLLBACK TRAN
+END CATCH
+
 
 --execute WarehouseManagerDB.dbo.AddClient @Company = 'Politechnika Wrocławska', @Nip = 8960005851, @Street = 'wybrzeże Wyspiańskiego' , @BuildingNumer = 27, 
 --@LocalNumber = NULL, @City = 'Wrocław', @ZIPCode = '50-370', @Email = 'pwr@pwr.edu.pl', @PhoneNumber = 48713202905;
