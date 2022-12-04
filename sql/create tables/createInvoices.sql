69ea32d80fe12b812933305ad8528ad8edbbb9ce
M B <mbagnsk@hotmail.com>
2022-12-04 22:44:31 +0100

    invoices.paymentdate allows null

--- sql/create tables/createInvoices.sql
+++ sql/create tables/createInvoices.sql
@@ -3,7 +3,7 @@ CREATE TABLE WarehouseManagerDB.dbo.INVOICES (
 	IDClient NUMERIC NOT NULL FOREIGN KEY REFERENCES WarehouseManagerDB.dbo.CUSTOMERS(IDClient),
 	OrderDatetime DATETIME NOT NULL,
 	DueDate DATE NOT NULL,
-	PaymentDate DATE NOT NULL,
+	PaymentDate DATE,
 	PaymentAmount NUMERIC(13,2) NOT NULL,
 	OrderActive BIT NOT NULL
 );
